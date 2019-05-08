fec8daa3c099f3e0eb25c527b0b19b359ef5366b
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-05-09 02:03:49 +0300

    Made changes to testFunc.pl

--- testFunc.pl
+++ testFunc.pl
@@ -2,9 +2,15 @@
 % test input for this : generateFinalKeywordList(['hello there', 'nice', 'car', 'very big cat', 'm m m m m']).
 
 generateFinalKeywordList([]).
-generateFinalKeywordList([H|T]) :-
-	tokenize_atom(H, WordComponents),
+generateFinalKeywordList([WH|T]) :-
+	tokenize_atom(WH,WordComponents),
 	length(WordComponents,Len),
 	write(Len),
+	nl,
+	( sub_string(case_insensitive,' ',WH)
+	-> write('Keyword is a phrase')
+	; write('Keyword is a single word')
+	),
+	nl,
 	nl,
 	generateFinalKeywordList(T).
