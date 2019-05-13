5a1c9022d1b6dee8a6e64658ac1281d4c2eff1d1
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-05-13 04:50:11 +0300

    Continued working on query.pl

---
+++ Finished_Functions/query.pl
@@ -0,0 +1,46 @@
+% test with: query([day-4,'general results-1',exceptions,airplane-1,'election vote','general meeting-2']).
+% or with: query([day-4,exceptions,'general meeting-2']).
+
+
+% Import generateFinalKeywordList.pl
+:- [generateFinalKeywordList].
+
+% Input: A list
+% Output: The same list, with all its elements converted to atoms
+prepareKeywordList([],[]).
+prepareKeywordList([H|T],FinalList) :-
+	prepareKeywordList(T,ListOfRest),
+	( atom(H)
+	% If current element is an atom
+	-> append([H],ListOfRest,FinalList)
+	% Else (if current element is not an atom)
+	; term_to_atom(H,AtomElement),
+	append([AtomElement],ListOfRest,FinalList)
+	).
+	
+	
+	
+	
+	
+
+query(UserInput) :-
+	% First, we convert all the compounds (for example, airplane-1 is recognised by prolog as a compound)
+	% from the user input to atoms, so that we can easily work with the list later. 
+	prepareKeywordList(UserInput,AtomList),
+	
+	% The prepared list that now contains only atom elements is passed
+	% to generateFinalKeywordList function.
+	generateFinalKeywordList(AtomList,FinalList),
+	nl,
+	nl,
+	write(FinalList).
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
