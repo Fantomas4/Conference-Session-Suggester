9ced1ddfc14c349a9dcc35c95d381d4cae311e9c
mitsaeirini <32482788+mitsaeirini@users.noreply.github.com>
2019-05-18 23:49:56 +0300

    Add files via upload

--- calculateScore.pl
+++ calculateScore.pl
@@ -4,26 +4,24 @@
 % Import sessions.pl
 :- [sessions].
  
-% calculateScore() gets the FinalList, abstracting head from tail, and returns the ListWithResults which is 
+% calculateScore() gets the FinalKeywords, abstracting head from tail and the FinalKeywordsPoints and returns the ListWithResults which is 
 % the list with titles of sessions and their final score
-calculateScore([],[]).
-calculateScore([H3|T3],ListWithResults):-
+calculateScore([],[],[]).
+calculateScore([H2|T2],[H3|T3],ListWithResults):-
 	% get from every session the title X and the list with topics 
 	session(X,[H1|T1]),
-	% get the user's input which is the list with the keywords
-	query([H2|T2]),
-	% Points is the last element of the current keyword 
-	last(H3,Points),
+	% call the query 
+	query(ListWithKeywords),
 	% check if the H2 (the user's input) exists in X (the title)
 	( sub_string(case_insensitive,H2,X)
 	->write('Keyword is in the title of session'),
-	ScoreOfTitle is Points*2 ),
+	ScoreOfTitle is H3*2 ),
 	% add in the new list (Score) the current ScoreOfTitle
 	append([],ScoreOfTitle,Score),
 	% check if the H2 (the user's input) exists in H1 (the topic)
 	( sub_string(case_insensitive,H2,H1)
 	->write('Keyword is in the topic of session'),
-	ScoreOfTopic is Points ),
+	ScoreOfTopic is H3 ),
 	% add in the new list (Score) the current ScoreOfTopic
 	append([],ScoreOfTopic,Score),
 	% find the max score of the list 'Score'
